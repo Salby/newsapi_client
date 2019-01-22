4fd985494867f420d6eb27eda6549e62b9ec375c
salby <srdlarsen@gmail.com>
2019-01-22 16:27:58 +0100

    Added countries and categories.

--- lib/newsapi_client.dart
+++ lib/newsapi_client.dart
@@ -1,3 +1,5 @@
 library newsapi_client;
 
 export 'src/client.dart';
+export 'src/categories.dart';
+export 'src/countries.dart';
---
+++ lib/src/categories.dart
@@ -0,0 +1,9 @@
+class Categories {
+  static String get business => 'business';
+  static String get entertainment => 'entertainment';
+  static String get general => 'general';
+  static String get health => 'health';
+  static String get science => 'science';
+  static String get sports => 'sports';
+  static String get technology => 'technology';
+}
