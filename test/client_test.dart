b0aea2dbea755aa79899c4cf8e21525744f246cc
salby <srdlarsen@gmail.com>
2019-02-01 12:35:20 +0100

    Updated tests to be up-to-date with the new request pattern.

--- test/client_test.dart
+++ test/client_test.dart
@@ -2,37 +2,33 @@ import 'package:test/test.dart';
 import 'package:newsapi_client/newsapi_client.dart';
 
 void main() {
-
-  NewsApiClient client = NewsApiClient('insert api key here');
+  NewsapiClient client = NewsapiClient('insert api key here');
 
   test('topHeadlines request', () async {
-    var response = await client.topHeadlines({
-      'category': Categories.business,
-    });
+    var response = await client.request(TopHeadlines(
+      category: Categories.business,
+    ));
     expect(response, containsPair('status', 'ok'));
   });
 
   test('everything request', () async {
-    var response = await client.everything({
-      'q': 'microsoft',
-    });
+    var response = await client.request(Everything(
+      query: 'Microsoft',
+    ));
     expect(response, containsPair('status', 'ok'));
   });
 
   group('sources', () {
-
     test('Sources request with parameters', () async {
-      var response = await client.sources(parameters: {
-        'country': Countries.norway,
-      });
+      var response = await client.request(Sources(
+        country: Countries.norway,
+      ));
       expect(response, containsPair('status', 'ok'));
     });
 
     test('Sources request without parameters', () async {
-      var response = await client.sources();
+      var response = await client.request(Sources());
       expect(response, containsPair('status', 'ok'));
     });
-
   });
-
 }
