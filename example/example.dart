34ffe11aab686b78fe73792f0c8a8cd8e9add102
salby <srdlarsen@gmail.com>
2019-02-02 07:27:13 +0100

    Rewrote example to actually work.

--- example/example.dart
+++ example/example.dart
@@ -1,9 +1,31 @@
 import 'package:newsapi_client/newsapi_client.dart';
 
-// Initialize client with your API key.
-final client = NewsapiClient('your API key');
+class News {
+  final client = NewsapiClient('YOUR API KEY');
 
-// Get top headlines from Canada.
-Future<Map<String, dynamic>> topHeadlinesCanada() async => client.topHeadlines({
-      'country': Countries.canada,
-    });
+  // Get the latest business headlines.
+  Future<Map<String, dynamic>> topHeadlines() async {
+    final response = await client.request(TopHeadlines(
+      category: Categories.business,
+    ));
+    return response;
+  }
+
+  // Get all articles with keyword: Microsoft from 2019-02-01 to now.
+  Future<Map<String, dynamic>> everything() async {
+    final response = await client.request(Everything(
+      query: 'Microsoft',
+      from: DateTime.parse('2019-02-01'),
+      to: DateTime.now(),
+    ));
+    return response;
+  }
+
+  // Get all norwegian sources.
+  Future<Map<String, dynamic>> sources() async {
+    final response = await client.request(Sources(
+      country: Countries.norway,
+    ));
+    return response;
+  }
+}
