94646a4c36c45398b23df6123e90cc5c6895f572
salby <srdlarsen@gmail.com>
2019-02-08 09:19:14 +0100

    Exported endpoint class.

--- lib/newsapi_client.dart
+++ lib/newsapi_client.dart
@@ -4,6 +4,7 @@ export 'src/client.dart';
 export 'src/categories.dart';
 export 'src/sort_by.dart';
 export 'src/countries.dart';
+export 'src/endpoint.dart';
 export 'src/endpoints/top_headlines.dart';
 export 'src/endpoints/everything.dart';
 export 'src/endpoints/sources.dart';
