b1247ccfade0de7305028b7b3be616df20e8cc77
callingmybluff <ismaeel.abuabdallah@gmail.com>
2019-10-16 19:48:20 +0200

    Turn into Generic news api

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,11 +1,12 @@
 import './endpoint.dart';
 
 class NewsapiClient {
-  NewsapiClient(this.apiKey) : assert(apiKey != null && apiKey.isNotEmpty);
+  NewsapiClient(this.apiKey, {this.baseUrl = 'https://newsapi.org/v2/'}) : assert(apiKey != null && apiKey.isNotEmpty && baseUrl != null && baseUrl.isNotEmpty);
 
   final String apiKey;
+  final String baseUrl;
 
   Future<Map<String, dynamic>> request(Endpoint endpoint) {
-    return endpoint.request(apiKey);
+    return endpoint.request(apiKey, baseUrl);
   }
 }
